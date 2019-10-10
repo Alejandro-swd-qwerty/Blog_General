58a31c7c0305c4b3b135061f27976c426a72e430
Eduardo-rico <emrs94@gmail.com>
2019-10-10 13:50:09 -0500

    [MOD] main.js se plantea la funcion para formatear entradas, y para cargar la vista

--- js/main.js
+++ js/main.js
@@ -7,3 +7,18 @@ function getPosts(){
         success: //se tiene que poner una función, cuando no hay errores. Esto debe de ser la plantilla para meter la colección de objetos
     });
 };
+
+/**
+ * function formatearEntradas(losPosts){
+ *      vaciar el contenedor para no appendear doble;
+ *      usar el metodo de JQuery $.each(losPosts, (index, valores) => {appendear el div o lo que sea, con el orden "index" y con los valores "valores"})
+ *          };
+ * 
+ * 
+ * 
+ * 
+ */
+
+function loadingView(viewUrl, funcionALlamar){
+    $(" la clase del contenedor").load(viewUrl,funcionALlamar); // se llama a la parte del wrapper o contenedor que va a estar cambiando, se llama a la función para obtener los posts o publicarlos, o eliminarlos, yo qé sé
+}
