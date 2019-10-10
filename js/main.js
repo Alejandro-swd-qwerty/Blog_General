10b0ee5ab32aace9985b54fe7c5a6f1ac89055e1
Eduardo-rico <emrs94@gmail.com>
2019-10-10 18:19:47 -0500

    [MOD] Aniadi la funcion que agarra los datos del modal

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ function getPosts(){
     $.ajax({ //se llama a Jquery y se usa el método ajax
         method:"GET", // el método para jalar los objetos es get
         url: "https://blog-5g.firebaseio.com/blog/posts/.json", //hay que poner la url, que puede ser  "https://blog-5g.firebaseio.com/blogGeneral/posts/.json" 
-        success: //se tiene que poner una función, cuando no hay errores. Esto debe de ser la plantilla para meter la colección de objetos
+        //success: //se tiene que poner una función, cuando no hay errores. Esto debe de ser la plantilla para meter la colección de objetos
     });
 };
 
